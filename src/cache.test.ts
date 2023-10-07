0901f33ad310bf46df186f7efd10a079a4947bba
Stiffler93 <sonntag19@gmail.com>
2023-10-08 01:29:01 +0200

    Set up jest tests.

---
+++ src/cache.test.ts
@@ -0,0 +1,150 @@
+import { cacheMe } from '.';
+
+jest.useFakeTimers();
+
+test('Cached function returns correct result', () => {
+    const cachedFunction = cacheMe(() => 1);
+
+    expect(cachedFunction()).toBe(1);
+    expect(cachedFunction()).toBe(1);
+    expect(cachedFunction()).toBe(1);
+});
+
+test('Cached function is actually only called once', () => {
+    const func = jest.fn();
+    const cachedFunction = cacheMe(func);
+
+    expect(func).toBeCalledTimes(0);
+    cachedFunction();
+    expect(func).toBeCalledTimes(1);
+    cachedFunction();
+    expect(func).toBeCalledTimes(1);
+});
+
+test('Cached function is called once for every unique parameter', () => {
+    const func = jest.fn((_: number) => {});
+    const cachedFunction = cacheMe(func);
+
+    expect(func).toBeCalledTimes(0);
+    cachedFunction(1);
+    expect(func).toBeCalledTimes(1);
+    cachedFunction(2);
+    expect(func).toBeCalledTimes(2);
+    cachedFunction(3);
+    expect(func).toBeCalledTimes(3);
+    cachedFunction(1);
+    expect(func).toBeCalledTimes(3);
+});
+
+test('Cache is warmed-up correctly', () => {
+    const func = jest.fn((_: number) => {});
+    cacheMe(func, {}, [1], [2], [3]);
+
+    expect(func).toBeCalledTimes(3);
+});
+
+test('Cache expires data correctly', () => {
+    const func = jest.fn();
+    const cachedFunction = cacheMe(func);
+
+    expect(func).toBeCalledTimes(0);
+    cachedFunction();
+    expect(func).toBeCalledTimes(1);
+    cachedFunction();
+    expect(func).toBeCalledTimes(1);
+
+    // advance timers to expire cache
+    jest.advanceTimersByTime(60_000);
+
+    cachedFunction();
+    expect(func).toBeCalledTimes(2);
+});
+
+test('Cached values TTL is reset on read', () => {
+    const func = jest.fn();
+    const cachedFunction = cacheMe(func, { expiration: { ttl: 2000, resetTTLOnRead: true } });
+
+    expect(func).toBeCalledTimes(0);
+    cachedFunction();
+    expect(func).toBeCalledTimes(1);
+    jest.advanceTimersByTime(1000);
+
+    cachedFunction();
+    expect(func).toBeCalledTimes(1);
+    jest.advanceTimersByTime(1000);
+
+    cachedFunction();
+    expect(func).toBeCalledTimes(1);
+    jest.advanceTimersByTime(1000);
+
+    cachedFunction();
+    expect(func).toBeCalledTimes(1);
+    jest.advanceTimersByTime(1000);
+
+    cachedFunction();
+    expect(func).toBeCalledTimes(1);
+});
+
+test('Cached values TTL is not reset on read when not configured', () => {
+    const func = jest.fn();
+    const cachedFunction = cacheMe(func, { expiration: { ttl: 2000, resetTTLOnRead: false } });
+
+    expect(func).toBeCalledTimes(0);
+    cachedFunction();
+    expect(func).toBeCalledTimes(1);
+    jest.advanceTimersByTime(1000);
+
+    cachedFunction();
+    expect(func).toBeCalledTimes(1);
+    jest.advanceTimersByTime(1000);
+
+    cachedFunction();
+    expect(func).toBeCalledTimes(2);
+});
+
+test('Cached values are refreshed periodically', () => {
+    const func = jest.fn();
+    const cachedFunction = cacheMe(func, { autoRefresh: { type: 'PERIODICALLY', interval: 1000 } });
+
+    expect(func).toBeCalledTimes(0);
+    cachedFunction();
+    expect(func).toBeCalledTimes(1);
+
+    jest.advanceTimersByTime(1000);
+    expect(func).toBeCalledTimes(2);
+
+    jest.advanceTimersByTime(1000);
+    expect(func).toBeCalledTimes(3);
+});
+
+test('Periodically refreshed values in cache keep referential integrity', () => {
+    const func = jest.fn();
+    const cachedFunction = cacheMe(func, { autoRefresh: { type: 'PERIODICALLY', interval: 1000 } });
+
+    expect(func).toBeCalledTimes(0);
+    const response1 = cachedFunction();
+    expect(func).toBeCalledTimes(1);
+
+    jest.advanceTimersByTime(1000);
+    expect(func).toBeCalledTimes(2);
+
+    const response2 = cachedFunction();
+    expect(func).toBeCalledTimes(2);
+
+    expect(response1 === response2).toBe(true);
+});
+
+test('Cached values update after read', () => {
+    const func = jest.fn();
+    const cachedFunction = cacheMe(func, { autoRefresh: { type: 'AFTER_READ' } });
+
+    expect(func).toBeCalledTimes(0);
+    cachedFunction();
+    expect(func).toBeCalledTimes(1);
+
+    cachedFunction();
+    expect(func).toBeCalledTimes(2);
+
+    cachedFunction();
+    expect(func).toBeCalledTimes(3);
+});
